d27c7bca90b7b9d8ac74180fd6cd65119b69e539
Abhijit2505 <abhijittripathy99@gmail.com>
2020-09-13 15:32:11 +0530

    Added a new method added a method to set Zero matrix

--- edualgo/algorithm.py
+++ edualgo/algorithm.py
@@ -726,3 +726,22 @@ class string_algorithms:
         Finally you have the desired rotated array.
         """
         print_msg_box(message)
+
+    def setZeros(self,matrix,row,column):
+        row_arr = [False] * row
+        col_arr = [False] * column
+        for i in range(row):
+            for j in range(column):
+                if(matrix[i][j] == 0):
+                    row_arr[i] = True
+                    col_arr[j] = True
+
+        for i in range(row):
+            if(row_arr[i]):
+                for j in range(column):
+                    matrix[i][j] = 0
+
+        for i in range(column):
+            if(row_arr[i]):
+                for j in range(row):
+                    matrix[j][i] = 0
