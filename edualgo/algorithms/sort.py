8d74ee3125e2a2b8d4e35019f12747b3c332302b
Abhijit Tripathy <52751963+Abhijit2505@users.noreply.github.com>
2020-10-07 08:32:51 +0530

    Update sort.py

--- edualgo/algorithms/sort.py
+++ edualgo/algorithms/sort.py
@@ -418,3 +418,30 @@ def merge_sort_hint():
     Learn More Here - https://en.wikipedia.org/wiki/Merge_sort
     """
     print_msg_box(message)
+  
+    # Quick Sort Algorithm     
+    def partition(arr, low, high):
+        i = (low-1)        
+        pivot = arr[high]     
+
+        for j in range(low, high):
+            if arr[j] <= pivot:
+               i = i+1
+               arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i+1], arr[high] = arr[high], arr[i+1]
+        return (i+1)
+
+
+    # Function to do Quick sort
+    def quickSort(arr, low, high):
+        if len(arr) == 1:
+            return arr
+        if low < high:
+
+
+            pi = partition(arr, low, high)
+
+            quickSort(arr, low, pi-1)
+            quickSort(arr, pi+1, high)
+
