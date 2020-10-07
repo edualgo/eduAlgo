c54ac10663e12b3d78b05be8911780d40862a718
Abhijit Tripathy <52751963+Abhijit2505@users.noreply.github.com>
2020-10-07 08:33:48 +0530

    Update sort.py

--- edualgo/algorithms/sort.py
+++ edualgo/algorithms/sort.py
@@ -419,29 +419,29 @@ def merge_sort_hint():
     """
     print_msg_box(message)
   
-    # Quick Sort Algorithm     
-    def partition(arr, low, high):
-        i = (low-1)        
-        pivot = arr[high]     
+# Quick Sort Algorithm     
+def partition(arr, low, high):
+    i = (low-1)        
+    pivot = arr[high]     
 
-        for j in range(low, high):
-            if arr[j] <= pivot:
-               i = i+1
-               arr[i], arr[j] = arr[j], arr[i]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+           i = i+1
+           arr[i], arr[j] = arr[j], arr[i]
 
-        arr[i+1], arr[high] = arr[high], arr[i+1]
-        return (i+1)
-
-
-    # Function to do Quick sort
-    def quickSort(arr, low, high):
-        if len(arr) == 1:
-            return arr
-        if low < high:
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
 
 
-            pi = partition(arr, low, high)
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
 
-            quickSort(arr, low, pi-1)
-            quickSort(arr, pi+1, high)
+
+        pi = partition(arr, low, high)
+
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
 
