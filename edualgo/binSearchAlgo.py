ef619886c98c24b333c003da3e92e8ed54f5aa29
Abhijit2505 <abhijittripathy99@gmail.com>
2020-11-21 09:28:15 +0530

    fixed a bug

--- edualgo/binSearchAlgo.py
+++ edualgo/binSearchAlgo.py
@@ -1,19 +1,5 @@
 from __init__ import print_msg_box
 
-def print_msg_box(msg, indent=1, width=None, title=None):
-    """Print message-box with optional title."""
-    lines = msg.split('\n')
-    space = " " * indent
-    if not width:
-        width = max(map(len, lines))
-    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
-    if title:
-        box += f'║{space}{title:<{width}}{space}║\n'  # title
-        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
-    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
-    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
-    print(box)
-
 class BinarySearyAlgorithm:
 
     #Define the bsearch method i.e. binary search
