c150c2381ef167a899ec52edff8ae519c29a9d3c
Suvan Sathyendira <46626983+suvanbalu@users.noreply.github.com>
2021-10-07 21:57:33 +0530

    Update stack_seq_finder.py

--- edualgo/algorithms/stack_seq_finder.py
+++ edualgo/algorithms/stack_seq_finder.py
@@ -1,37 +1,36 @@
-def stack_sequence(pushes:list , pops:list)->list:
+def stack_sequence(pushes : list , pops : list):
     '''
-    Function to check whether the given sequence of pop can be achieved from the given push sequences 
-    in a stack. eg- push -> 1 2 3 4 5 , pop -> 3 2 1 5 4 is a valid sequence and the output will be 
-    ['Push[1]', 'Push[2]', 'Push[3]', 'Push[4]', 'Push[5]', 'Pop[5]', 'Pop[4]', 'Pop[3]', 'Pop[2]', 'Pop[1]']
+ Function to check whether the given sequence of pop can be 
+ achieved from the given push sequences 
+ in a stack. eg- push -> 1 2 3 4 5 , pop -> 3 2 1 5 4 is a valid sequence 
     '''
     stack = []
-    sequence=[]
+    sequence = []
     i = 0
-    l = 0
+    j = 0
     for n in pushes:
         stack.append(n)
         sequence.append(f"Push[{n}]")
-        l += 1
+        j += 1
         while l and stack[-1] == pops[i]:
             sequence.append(f"Pop[{stack.pop()}]")
             i += 1
-            l -= 1
-    return(sequence)
+            j -= 1
+    return sequence
+
 
 if __name__ == "__main__":
-    ''''
-    Input format -> 1st line : integers with space
-                    2nd line : integers with space
-                    length(pushes)==length(pops)
-    Output format -> Valid push and pops of the stack to achieve the pop sequence 
-                    if no valid sequence found print The sequences isn't valid
     '''
-    pushes = list(map(int,input().split()))
-    pops = list(map(int,input().split()))
-    output = stack_sequence(pushes,pops)
-    if len(output)==2*len(pushes):
+Input format -> 1st line : integers with space
+2nd line : integers with space
+length(pushes)==length(pops)
+    '''
+    pushesin = list(map(int , input().split()))
+    popsin = list(map(int , input().split()))
+    output = stack_sequence(pushesin , popsin)
+    if len(output) == 2*len(pushes):
         print("")
-        for i in output:
-            print(i,end=" ")
+        for sequnce in output:
+            print(sequnce , end=" ")
     else:
-        print("The sequences isn't valid")
+        print("The sequences isn't valid\n")
