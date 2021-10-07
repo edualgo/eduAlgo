fa81d41a964931925f0b0263eb1d8110c6bc67e2
Suvan Sathyendira <46626983+suvanbalu@users.noreply.github.com>
2021-10-07 22:03:54 +0530

    Update stack_seq_finder.py

--- edualgo/algorithms/stack_seq_finder.py
+++ edualgo/algorithms/stack_seq_finder.py
@@ -1,9 +1,4 @@
 def stack_sequence(pushes : list , pops : list):
-    '''
- Function to check whether the given sequence of pop can be 
- achieved from the given push sequences 
- in a stack. eg- push -> 1 2 3 4 5 , pop -> 3 2 1 5 4 is a valid sequence 
-    '''
     stack = []
     sequence = []
     i = 0
@@ -12,7 +7,7 @@ def stack_sequence(pushes : list , pops : list):
         stack.append(n)
         sequence.append(f"Push[{n}]")
         j += 1
-        while l and stack[-1] == pops[i]:
+        while j and stack[-1] == pops[i]:
             sequence.append(f"Pop[{stack.pop()}]")
             i += 1
             j -= 1
@@ -20,15 +15,13 @@ def stack_sequence(pushes : list , pops : list):
 
 
 if __name__ == "__main__":
-    '''
-Input format -> 1st line : integers with space
-2nd line : integers with space
-length(pushes)==length(pops)
-    '''
+    #Input format -> 1st line : integers with space
+    #2nd line : integers with space
+    #length(pushes)==length(pops)
     pushesin = list(map(int , input().split()))
     popsin = list(map(int , input().split()))
     output = stack_sequence(pushesin , popsin)
-    if len(output) == 2*len(pushes):
+    if len(output) == 2*len(pushesin):
         print("")
         for sequnce in output:
             print(sequnce , end=" ")
